e532c46a541c17a221e1ca22edf8a33caa184772
Danny Mauricio <danny.montero@gmail.com>
2019-03-06 22:06:17 -0500

    Agregar archivos de proyecto.

---
+++ EatMore.API/Startup.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GraphiQl;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EatMore.API
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddDbContext<AppContext>(opt => opt.UseInMemoryDatabase("EatMoreDb"));
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseGraphiQl();
+
+            using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+
+                var db = serviceScope.ServiceProvider.GetService<AppContext>();
+                DataSeed.Run(db);
+            }
+
+            app.UseMvc();
+        }
+    }
+}
